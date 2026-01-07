fd7244da543610bc74527546a5e12d0ec9a1ab3c
Lee <jeremlee.global@gmail.com>
2026-01-07 23:39:16 +0530

    Update type.js

--- type.js
+++ type.js
@@ -1,1 +1,13 @@
+// Simple JavaScript program
+
+// Print a message
+console.log("Hello, JavaScript!");
+
+// Add two numbers
+let a = 5;
+let b = 3;
+let sum = a + b;
+
+// Display the result
+console.log("The sum is: " + sum);
 
