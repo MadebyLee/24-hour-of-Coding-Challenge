ea6e60f0d3e307098bfcf4b9fa627833a09adf23
Lee <jeremlee.global@gmail.com>
2026-01-07 23:36:35 +0530

    Update ui.dart

--- ui.dart
+++ ui.dart
@@ -1,1 +1,15 @@
+// Simple Dart program
+
+void main() {
+  // Print a message
+  print("Hello, Dart!");
+
+  // Add two numbers
+  int a = 5;
+  int b = 3;
+  int sum = a + b;
+
+  // Display the result
+  print("The sum is: $sum");
+}
 
